aa49c67daeb9488bdc10fb8a158e541b45ef1663
Ben Goss <ben.goss@student.hu.nl>
2018-06-18 17:27:25 +0200

    initial3

--- src/main/java/webservice/ServiceProvider.java
+++ src/main/java/webservice/ServiceProvider.java
@@ -1,9 +1,8 @@
 package webservice;
 
 public class ServiceProvider {
-	private static RecipeService recipeService = new RecipeService();
 
 	public static RecipeService getRecipeService() {
-		return recipeService;
+		return new RecipeService();
 	}
 }
