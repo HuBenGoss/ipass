36cff0029d47d1bcbe2ce5aa93eccf46faa1b533
Ben Goss <ben.goss@student.hu.nl>
2018-06-19 16:44:02 +0200

    initial3

--- src/main/java/persistence/PostgresBaseDao.java
+++ src/main/java/persistence/PostgresBaseDao.java
@@ -1,19 +1,26 @@
 package persistence;
 
-import java.sql.Connection;
+
 
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class PostgresBaseDao {
     protected final Connection getConnection() {
         Connection result = null;
 
         try {
-            InitialContext ic = new InitialContext();
-            DataSource datasource = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+            Class.forName("org.postgresql.Driver");
 
-            result = datasource.getConnection();
+//            InitialContext ic = new InitialContext();
+//            DataSource datasource = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
+//
+//            result = datasource.getConnection();
+//            source.getConnection();
+            result = DriverManager.getConnection(
+                    "jdbc:postgresql://ec2-79-125-6-160.eu-west-1.compute.amazonaws.com:5432/d4235js315g98f?ssl=true%26sslfactory=org.postgresql.ssl.NonValidatingFactory","eqegubxfglkhao", "ffbeb1ea8d91bb69f731b11f5637303b212730b6bcd6d78ce5c706482cbb558e");
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
